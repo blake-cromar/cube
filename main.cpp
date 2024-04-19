4a6f70c97d62873819a47951f52a79c12832cc6a
Blake Cromar <blake.cromar@icloud.com>
2024-04-19 15:40:58 +0200

    Moved the information of main to it's own file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include "include/activation_functions.hpp"
+using namespace std;
+
 int main () {
+    double test_values[] = {0.0, 1.0, -1.0, 2.0, -2.0};
+
+    cout << "Testing hyperbolic tangent function:" << endl;
+    for (double value : test_values) {
+        double result = hyperbolic_tangent(value);
+        cout << "tanh(" << value << ") = " << result << endl;
+    }
+
     return 0;
 }
